8634ddd5d5c31e3e8bedc239faaeb59978915f60
Haimen <haimenabbas@gmail.com>
2025-12-10 10:27:42 +0100

    added operations to cropRepository

--- cropservice/src/main/java/se/yrgo/cropservice/data/CropRepository.java
+++ cropservice/src/main/java/se/yrgo/cropservice/data/CropRepository.java
@@ -3,8 +3,18 @@ package se.yrgo.cropservice.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import se.yrgo.cropservice.entities.Crop;
+import se.yrgo.cropservice.entities.enums.PlantType;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CropRepository extends JpaRepository<Crop, Long> {
+
+    List<Crop> findByPlantType(PlantType plantType);
+
+    Optional<Crop> findByName(String name);
+
+    Optional<Crop> findByLatinName(String latinName);
 
 }
